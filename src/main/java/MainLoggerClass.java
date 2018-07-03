272589695b1eea09b532e44116552ba892f08ef3
zeki.tel <zeki.tel34@gmail.com>
2018-07-03 15:35:11 +0300

    new version

--- src/main/java/MainLoggerClass.java
+++ src/main/java/MainLoggerClass.java
@@ -6,10 +6,15 @@ public class MainLoggerClass {
     public static final Logger logger=LoggerFactory.getLogger(MainLoggerClass.class);
 
     public static void main(String[] args) {
-        String name="zeki.tel";
+        String name="FURKAN";
         logger.debug("Hi {}"+name);
         logger.info(name);
         logger.warn("uyarı mesajı");
         logger.error("hata mesajı");
+        logger.info("Request Fields: id durum");
+        logger.info("Request Fields: id ");
+        logger.info("Request Fields: id ");
+        logger.info("Request Fields: id ");
+        logger.info("Request Fields: id  acliliyetBilgisi");
     }
 }
